7d18079242ebc06d8b80fc1e74d3e45ed10135cd
Gael Chavez <gael.chavez.h@gmail.com>
2024-10-17 11:36:42 -0400

    Commiting new file (scene clone) -mac

---
+++ src/Game_engine/Screen.java
@@ -0,0 +1,15 @@
+package Game_engine;
+
+import Cards.Card;
+import UI.ImageCreation;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.awt.GLJPanel;
+import com.jogamp.opengl.util.texture.Texture;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen {
+
+}
